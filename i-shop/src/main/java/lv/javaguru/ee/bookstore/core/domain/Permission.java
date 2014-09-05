4d3d4a7aabdd8dcd005477159bda1dd94202b6b4
Nikolay <nikolay.borovoy@gmail.com>
2014-09-05 10:33:51 +0300

    bookstore domain+maven depend

---
+++ i-shop/src/main/java/lv/javaguru/ee/bookstore/core/domain/Permission.java
@@ -0,0 +1,42 @@
+package lv.javaguru.ee.bookstore.core.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MumboJumbo
+ * Date: 05/09/14
+ * Time: 10:19
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "permission" }) })
+public class Permission implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String permission;
+
+    Permission() {
+        // Form ORM
+    }
+
+    public Permission(String permission) {
+        this.permission = permission;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+}
