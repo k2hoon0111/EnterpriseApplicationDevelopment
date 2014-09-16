d9c4864bd81b68bfb6fa2280504a48514869798b
yuri-d <yuri.sphere@gmail.com>
2014-09-16 20:10:28 +0300

    domain unfinished

--- warehouse/src/main/java/lv/javaguru/ee/warehouse/core/domain/Order.java
+++ warehouse/src/main/java/lv/javaguru/ee/warehouse/core/domain/Order.java
@@ -1,11 +1,52 @@
 package lv.javaguru.ee.warehouse.core.domain;
 
+import javax.persistence.*;
+
 /**
  * Created by Yuri D. on 2014.09.08..
  */
+
+
+@Entity
+@Table(name = "orders")
 public class Order {
-    Long id;
-    Long productId;
-    Long clientId;           // product ordered for user
-    int itemsQuantity;      // how many items to order
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "orderId")
+    private Long orderId;
+    private Long productId;         // isbn
+    private Long clientId;           // product ordered for user
+    private int itemsQuantity;      // how many items to order
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getItemsQuantity() {
+        return itemsQuantity;
+    }
+
+    public void setItemsQuantity(int itemsQuantity) {
+        this.itemsQuantity = itemsQuantity;
+    }
 }
