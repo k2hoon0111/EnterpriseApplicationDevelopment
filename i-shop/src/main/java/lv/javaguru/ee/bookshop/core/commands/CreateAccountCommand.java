49015c203dfe80abb9c7dd3dfb214a066c6b88ca
Nikolay <nikolay.borovoy@gmail.com>
2014-09-22 23:08:28 +0300

    Account REST

---
+++ i-shop/src/main/java/lv/javaguru/ee/bookshop/core/commands/CreateAccountCommand.java
@@ -0,0 +1,134 @@
+package lv.javaguru.ee.bookshop.core.commands;
+
+import java.util.Date;
+
+/**
+ * Created by MumboJumbo on 22/09/14.
+ */
+public class CreateAccountCommand implements DomainCommand<CreateAccountResult> {
+
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String emailAddress;
+    private String username;
+    private String password;
+
+    private String street;
+    private String houseNumber;
+    private String boxNumber;
+    private String city;
+    private String postalCode;
+    private String country;
+
+    public CreateAccountCommand(String firstName, String lastName, Date dateOfBirth, String emailAddress, String username, String password, String street, String houseNumber, String boxNumber, String city, String postalCode, String country) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.emailAddress = emailAddress;
+        this.username = username;
+        this.password = password;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.boxNumber = boxNumber;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getBoxNumber() {
+        return boxNumber;
+    }
+
+    public void setBoxNumber(String boxNumber) {
+        this.boxNumber = boxNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
